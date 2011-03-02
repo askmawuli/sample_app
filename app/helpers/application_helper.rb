ae5805d3507e8d26a5a1271b62a5186939f80579
askmawuli <mawuli.sikanku@gmail.com>
2011-03-02 11:39:33 +0000

    chapter 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	
+	#Return a title on a per-page basis
+	def title
+		base_title = "Ruby on Rails Tutorial Sample App"
+		return base_tile if @title.nil?
+		return "#{base_title} | #{@title}"		
+
+	end
 end
